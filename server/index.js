6e4b1b9af5f0eab30329c024c7e0e48cd88952d7
Arzan101 <khanazzu994@gmail.com>
2022-06-25 18:51:42 +0530

    commit on master

--- server/index.js
+++ server/index.js
@@ -6,3 +6,5 @@ import cors from 'cors';
 const app = express();
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
