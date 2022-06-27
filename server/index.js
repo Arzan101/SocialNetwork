bd61d28fb78a572e1f02681fcdb45640b3da6fa7
Arzan101 <khanazzu994@gmail.com>
2022-06-27 09:04:43 +0530

    commit on master

--- server/index.js
+++ server/index.js
@@ -8,3 +8,7 @@ const app = express();
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://socialnetwork:socialnetwork@cluster0.1np1skv.mongodb.net/?retryWrites=true&w=majority';
+
+const PORT = 5000;
