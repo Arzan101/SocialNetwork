450ac1132b08c75c6fcee2e530f406a73d9676e9
Arzan101 <khanazzu994@gmail.com>
2022-08-06 21:57:59 +0530

    commit on master

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,8 @@
-// import logo from './logo.svg';
-// import './App.css';
 import React from 'react';
-
+import Navbar from './components/Navbar';
 function App() {
   return (
-   <div>
-   <h1>App</h1>
-
-   </div>
+   <Navbar />
   );
 }
 
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sign In</a></li>
+        <li><a href="badges.html">Sign Up</a></li>
+        <li><a href="collapsible.html">Profile</a></li>
+      </ul>
+    </div>
+  </nav>
+        
+  )
+}
+
+export default Navbar
