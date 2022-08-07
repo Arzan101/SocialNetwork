b28457b10c10d89345d78486e111df2a2126f954
Arzan101 <khanazzu994@gmail.com>
2022-08-07 10:59:54 +0530

    commit on master

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <nav>
     <div className ="nav-wrapper blue">
-      <Link to = "/" className ="brand-logo left">SocialNetwork</Link>
+      <Link to = "/" className ="brand-logo left">InstaBook</Link>
       <ul id="nav-mobile" className="right">
         <li><Link to="/signin">Sign In</Link></li>
         <li><Link to="/signup">Sign Up</Link></li>
