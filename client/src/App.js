e7298cc223c7270cdfda472332516427ffee8e34
Arzan101 <khanazzu994@gmail.com>
2022-08-07 10:55:41 +0530

    commit on master

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar';
 import './App.css'
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from './components/screens/Home.js';
+import Signin from './components/screens/Signin.js'
+import Signup from './components/screens/Signup.js'
+import Profile from './components/screens/Profile.js'
 
 
 function App() {
@@ -11,6 +14,9 @@ function App() {
         <Navbar />
          <Routes>
            <Route path="/" element={<Home/>}/>
+           <Route path="/signin" element={<Signin/>}/>
+           <Route path="/signup" element={<Signup/>}/>
+           <Route path="/profile" element={<Profile/>}/>
          </Routes>
     </BrowserRouter>
   );
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
     <nav>
     <div className ="nav-wrapper blue">
-      <a href="/" className ="brand-logo left">SocialNetwork</a>
+      <Link to = "/" className ="brand-logo left">SocialNetwork</Link>
       <ul id="nav-mobile" className="right">
-        <li><a href="sass.html">Sign In</a></li>
-        <li><a href="badges.html">Sign Up</a></li>
-        <li><a href="collapsible.html">Profile</a></li>
+        <li><Link to="/signin">Sign In</Link></li>
+        <li><Link to="/signup">Sign Up</Link></li>
+        <li><Link to="/profile">Profile</Link></li>
       </ul>
     </div>
   </nav>
