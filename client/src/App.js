ae3af72ca6867b9e335521bf3bd88dae80e6c4c4
Arzan101 <khanazzu994@gmail.com>
2022-08-07 10:12:45 +0530

    commit on master

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import './App.css'
+import {BrowserRouter, Route} from 'react-router-dom';
+import home from './components/screens/Home.js'
 
 
 function App() {
