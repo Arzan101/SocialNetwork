34ccc60b079c52e8cf95aceb1145edffdb174d2b
Arzan101 <khanazzu994@gmail.com>
2022-08-08 23:26:35 +0530

    commit on master

--- client/src/components/screens/Signin.js
+++ client/src/components/screens/Signin.js
@@ -3,11 +3,11 @@ import React from 'react'
 function Signin() {
   return (
   <div className='mycard'>
-    <div className='card auth-card'>
+    <div className='card auth-card input-field' >
        <h2>InstaBook</h2>
        <input type="text" placeholder="email"/>
        <input type="text" placeholder="password"/>
-       <button class="btn waves-effect waves-light" type="submit" name="action">Submit</button>
+       <button class="btn waves-effect waves-light #2196f3 blue" type="submit" name="action">Submit</button>
     </div>
   </div>
   )
