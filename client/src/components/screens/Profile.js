f52339420cc4cf1535e07c3944c2112ead5c7f70
Arzan101 <khanazzu994@gmail.com>
2022-08-10 20:36:20 +0530

    commit on master

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -3,10 +3,33 @@ import React from 'react'
 function Profile() {
   return (
     <div>
-     <div>
+     <div
+     style={
+      {
+       display:'flex',
+       justifyContent:"space-around",
+       margin:"15px 0px"
+      }
+     }
+     >
        <div>
-       
-        <img src="https://images.unsplash.com/photo-1660134149224-bf526f2f064a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="profile"/>
+
+        <img
+        
+        style={
+          {
+            width:"160px",
+            height:"160px",
+            borderRadius:"80px",
+
+          }
+
+        }
+        src="https://images.unsplash.com/photo-1655114722721-5c75114be5ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="profile"/>
+       </div>
+
+       <div>
+        <h4>Arzan Khan</h4>
        </div>
      </div>
     </div>
