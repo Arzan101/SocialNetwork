12d4cda12449e669bc6daf6b76c0686de276b18a
Arzan101 <khanazzu994@gmail.com>
2022-08-10 20:24:01 +0530

    commit on master

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Profile() {
   return (
-    <div>Profile</div>
+    <div>
+     <div>
+       <div>
+       
+        <img src="https://images.unsplash.com/photo-1660134149224-bf526f2f064a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="profile"/>
+       </div>
+     </div>
+    </div>
   )
 }
 
