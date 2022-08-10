c17ea235291895b6386818dc31d28aefe95459d1
Arzan101 <khanazzu994@gmail.com>
2022-08-10 20:03:00 +0530

    commit on master

--- client/src/components/screens/Signin.js
+++ client/src/components/screens/Signin.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Signin() {
   return (
@@ -7,7 +8,11 @@ function Signin() {
        <h2>InstaBook</h2>
        <input type="text" placeholder="email"/>
        <input type="text" placeholder="password"/>
-       <button class="btn waves-effect waves-light #2196f3 blue" type="submit" name="action">Submit</button>
+       <button class="btn waves-effect waves-light #2196f3 blue" type="submit" name="action">
+        Sign In</button>
+        <h6>
+        <Link to ='/signup'> Register to create account</Link>
+         </h6>
     </div>
   </div>
   )
--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Signup() {
   return (
-    <div>Signup</div>
-  )
+    <div className='mycard'>
+      <div className='card auth-card input-field' >
+         <h2>InstaBook</h2>
+         <input type="text" placeholder="Name"/>
+         <input type="text" placeholder="email"/>
+         <input type="text" placeholder="password"/>
+         <button class="btn waves-effect waves-light #2196f3 blue" type="submit" name="action">Sign Up</button>
+         <h6>
+         <Link to ='/Signin'>Already have an account ?</Link>
+         </h6>
+      </div>
+    </div>
+    )
 }
 
 export default Signup
