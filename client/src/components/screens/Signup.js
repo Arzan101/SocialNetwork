73ea6adc47eedd7cd5e4e2a478caf87cb9938f3c
Arzan101 <khanazzu994@gmail.com>
2022-08-15 18:31:47 +0530

    commit on master

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -10,9 +10,9 @@ function Signup() {
     <div className='mycard'>
       <div className='card auth-card input-field' >
          <h2>InstaBook</h2>
-         <input type="text" placeholder="Name"/>
-         <input type="text" placeholder="email"/>
-         <input type="text" placeholder="password"/>
+         <input type="text" placeholder="Name" value={name} />
+         <input type="text" placeholder="email" value={email}/>
+         <input type="text" placeholder="password" value={password}/>
          <button class="btn waves-effect waves-light #2196f3 blue" type="submit" name="action">Sign Up</button>
          <h6>
          <Link to ='/Signin'>Already have an account ?</Link>
