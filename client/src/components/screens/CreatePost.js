a6db799759c4c7f230be4036ce246a0ec9c846bb
Arzan101 <khanazzu994@gmail.com>
2022-09-17 12:26:10 +0530

    commit on master

--- client/src/components/screens/CreatePost.js
+++ client/src/components/screens/CreatePost.js
@@ -1,60 +1,96 @@
-//Client/src/compenents/screens/CreatePost.js
-import React, { useState } from "react";
+import React, { useEffect, useState } from 'react';
+import M from 'materialize-css';
+import { useNavigate } from 'react-router-dom';
 
 function CreatePost() {
-      const[title,setTitle]=useState("");
-      const[body,setBody]=useState("");
-      const[image,setImage]=useState("");
+    const [title,setTitle] = useState("");
+    const [body,setBody] = useState("");
+    const [image,setImage] = useState("");
+    const [url,setUrl] = useState("");
+    const navigate = useNavigate();
+   
+    useEffect(()=>{
+        fetch("/createpost",{
+            method:"post",
+            headers: {
+                "Content-Type":"application/json",
+                "Authorization": "Bearer "+localStorage.getItem("jwt")
+            },
+            body:JSON.stringify({
+                title:title,
+                body:body,
+                pic:url
+            })
+        }).then(res=>res.json)
+        .then(data=>{ 
+            console.log(data);
+            if(data.error){
+              M.toast({html: data.error, classes:"#b71c1c red darken-4"})
+            }
+            else{
+              M.toast({html:"Successully Post Created", classes:"#43a047 green darken-1"})
+            }
+          }).catch(err=>{
+            console.log(err)
+          })
+    },[body, navigate, title, url])
 
-      const postDetails=()=>{
+    const postDetails= ()=>{
         const data = new FormData();
         data.append("file",image);
         data.append("upload_preset","instabooksite");
-        fetch("https://api.cloudinary.com/v1_1/instabookcloud101/image/upload/"
-        ,{
-          method:"post",
-          body:data })
-          .then(res=>res.json())
-          .then(data=>{
-            console.log(data)
-          })
-          .catch(err=>{
-            console.log(err);
-          })
         
-      }
+        fetch("https://api.cloudinary.com/v1_1/instabookcloud101/image/upload/",{
+        method:"post",
+        body:data })
+        .then(res=>res.json())
+        .then(data=>{
+             //console.log(data);
+             setUrl(data.url);
+        })
+        .catch(err=>{
+            console.log(data);
+        })
+    }
+
   return (
-    <div className="card input-field auth-card">
-
-      <input 
-       type="text" 
-       placeholder="Title" 
-       value={title}
-       onChange={(e)=>setTitle(e.target.value)}
-      />
-      <input
-       type="text" 
-       placeholder="body" 
-       value={body}
-       onChange={(e)=>setBody(e.target.value)}
-       />
-
-      <div className="file-field input-field">
-        <div className="btn">
-          <span>UPLOAD IMAGE</span>
-          <input
-           type="file"
-           onChange={(e)=> setImage(e.target.files)[0]}
-          />
+    <div className='card input-field'
+    style={{
+        margin: "30px auto",
+        maxWidth: "500px",
+        padding: "20px",
+        textAlign: "center"
+    }}
+    >
+        <input 
+        type="text" 
+        placeholder='title'
+        value={title}
+        onChange={(e)=>setTitle(e.target.value)}
+        />
+        <input 
+        type="text" 
+        placeholder='body'
+        value={body}
+        onChange={(e)=>setBody(e.target.value)}
+        />
+        <div className="file-field input-field">
+            <div className="btn">
+                <span>Upload Image</span>
+                <input 
+                type="file" 
+                onChange={(e)=>setImage(e.target.files[0])}
+                />
+            </div>
+            <div className="file-path-wrapper">
+                <input className="file-path validate" type="text" />
+            </div>
         </div>
-        <div className="file-path-wrapper">
-          <input className="file-path validate" type="text" />
-        </div>
-      </div>
-      <button class="btn waves-effect waves-light blue" onClick={()=>postDetails()}>Submit Post</button>
-
+        
+        <button className="btn waves-effect waves-light #2196f3 blue darken-1"
+         onClick={()=>postDetails()}>Submit Post</button>
     </div>
-  );
+  )
 }
 
-export default CreatePost;
+export default CreatePost
