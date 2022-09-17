e7015960b8d7091171be6f731a3a038b5b8d33cd
Arzan101 <khanazzu994@gmail.com>
2022-09-17 11:40:43 +0530

    commit on master

--- client/src/components/screens/CreatePost.js
+++ client/src/components/screens/CreatePost.js
@@ -15,6 +15,13 @@ function CreatePost() {
         fetch("https://api.cloudinary.com/instabookcloud101/image/upload/",{
           method: "post",
           body:data })
+          .then(res=>res.json())
+          .then(data=>{
+            console.log(data)
+          })
+          .catch(err=>{
+            console.log(err);
+          })
         
       }
   return (
