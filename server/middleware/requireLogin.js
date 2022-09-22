bcb9707f0a50426ea89ec97ebc614ab97ca0ffd3
Arzan101 <khanazzu994@gmail.com>
2022-09-22 23:17:33 +0530

    commit on master

--- server/middleware/requireLogin.js
+++ server/middleware/requireLogin.js
@@ -1,9 +1,18 @@
-export const requireLogin=(req,res,next)=>{
+import jwt from 'jsonwebtoken'
+import { SecretValues } from "../keys.js";
+
+export const requireLogin=(req,res,next
+    )=>{
  const {authorization} = req.headers;
 
  if(!authorization){
     return res.status(401).json({error:"you must be Logged In!"})
  }
+
+ const token=authorization.replace("Bearer","")
+ jwt.verify(token,SecretValues,(err,payload)=>{
+
+ })
 
 
 }
