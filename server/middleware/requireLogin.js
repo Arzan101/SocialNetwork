d77419ebbd5c34648866ce93cc215b9dac4b0ac1
Arzan101 <khanazzu994@gmail.com>
2022-09-22 22:28:23 +0530

    commit on master

--- server/middleware/requireLogin.js
+++ server/middleware/requireLogin.js
@@ -1,3 +1,7 @@
-export const requireLogin=()=>{
-    
+export const requireLogin=(req,res,next)=>{
+ const {authorization} = req.headers;
+
+ if(!authorization){
+    res.status(401).json({error:"you must be Logged In!"})
+ }
 }
