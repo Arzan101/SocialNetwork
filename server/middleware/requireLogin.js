c772df88cf98b81543ef3230a0d1a297829d610e
Arzan101 <khanazzu994@gmail.com>
2022-09-24 11:04:45 +0530

    commit on master

--- server/middleware/requireLogin.js
+++ server/middleware/requireLogin.js
@@ -17,9 +17,9 @@ export const requireLogin=(req,res,next)=>
    }
   const {_id}= payload;
   User.findById(_id).then(userdata=>{
-    req.user=userdata 
+    req.user=userdata;
    })
- next()
+ next();
 })
 
 }
