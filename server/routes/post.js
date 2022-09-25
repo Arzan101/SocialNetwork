a93819c0091e825c9e339629f9d548c1e288c704
Arzan101 <khanazzu994@gmail.com>
2022-09-25 14:44:50 +0530

    commit on master

--- server/routes/post.js
+++ server/routes/post.js
@@ -4,5 +4,10 @@ import { requireLogin } from "../middleware/requireLogin";
 const router = express.Router()
 
 router.post('/createpost',requireLogin,(req,res)=>{
+    const {title,body}=req.body;
+    if(!title || !body){
+        return res.status(422).json({error:"Please add all details"});
+    }
+
 
 })
