babd4ab720b2a023a0060792919219ef3e9908c3
Arzan101 <khanazzu994@gmail.com>
2022-09-25 14:32:23 +0530

    commit on master

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,3 +1,8 @@
 import express from "express";
+import { requireLogin } from "../middleware/requireLogin";
 
 const router = express.Router()
+
+router.post('/createpost',requireLogin,(req,res)=>{
+
+})
