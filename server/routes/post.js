e121b71f6c0dc4b1f7990e193f80bd0c5cf88fc6
Arzan101 <khanazzu994@gmail.com>
2022-09-25 14:52:29 +0530

    commit on master

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 import { requireLogin } from "../middleware/requireLogin";
 
 const router = express.Router()
@@ -6,8 +7,19 @@ const router = express.Router()
 router.post('/createpost',requireLogin,(req,res)=>{
     const {title,body}=req.body;
     if(!title || !body){
-        return res.status(422).json({error:"Please add all details"});
+        return res.status(422).json({error:"Please add all fields"});
     }
 
+    const post = new post({
+        title:title,
+        body:body,
+        postedBy:re.user
+    })
 
+    post.save().then(result=>{
+        res.json({post:result})
+    })
+    .catch(err=>{
+        console.log(err)
+    })
 })
