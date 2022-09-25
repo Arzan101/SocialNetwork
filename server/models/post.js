d1146240625e7bf1f7848327ffd69d8b22910a04
Arzan101 <khanazzu994@gmail.com>
2022-09-25 13:55:27 +0530

    commit on master

--- server/models/post.js
+++ server/models/post.js
@@ -1,3 +1,19 @@
 import mongoose from "mongoose";
 
-const postSchema = new mongoose.Schema(); 
+const postSchema = new mongoose.Schema({
+    title: {
+        type:String,
+        required:true
+    },
+    body:{
+        type:String,
+        required:true
+    },
+    photo:{
+        type:String,
+        required:true
+    }
+}); 
+
+
+mongoose.model("Post",postSchema);
