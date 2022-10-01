5c6af31631c8dc0045f6cfd5b6036dbac0137c29
Arzan101 <khanazzu994@gmail.com>
2022-10-01 13:45:24 +0530

    commit on master

--- server/routes/post.js
+++ server/routes/post.js
@@ -7,7 +7,7 @@ const routerPost = express.Router()
 const Post = mongoose.model("Post");
 
 routerPost.post('/createpost',requireLogin,(req,res)=>{
-    const {title,body}=req.body;
+    const {title,body,pic}=req.body;
     if(!title || !body){
         return res.status(422).json({error:"Please add all fields"});
     }
@@ -15,6 +15,7 @@ routerPost.post('/createpost',requireLogin,(req,res)=>{
     const post = new post({
         title:title,
         body:body,
+        photo:pic,
         postedBy:req.user
     })
 
